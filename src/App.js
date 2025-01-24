a350ba02ba18551fa85859efd2923453927deacc
glizzygobblrr <S10257120@connect.np.edu.sg>
2025-01-25 00:07:08 +0800

    file upload to cloud and fetch advertisements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // App.jsx
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import TemplateEditor from './components/templateEditor';
-import FileUpload from './components/FileUpload';
+import TemplateEditor from './components/AdForm';
+import TemplateManagement from './components/AdList';
 import TemplatePage from './components/templatePage';
 import ViewAllTemplates from "./components/viewAllTemplates";
 import AdvertisementDisplay from "./components/advertisement"; // Import the component
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/manage-templates" element={<TemplatePage />} />
         <Route path="/manage-templates/view-all" element={<ViewAllTemplates />} />
-        <Route path="/file-management" element={<FileUpload />} />
+        <Route path="/template-management" element={<TemplateManagement />} />
         <Route path="/template-editor" element={<TemplateEditor/>} />
         <Route path="/advertisement-display" element={<AdvertisementDisplay />} /> {/* New route */}
         {/* Default route for undefined paths */}
