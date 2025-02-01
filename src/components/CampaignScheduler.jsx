4aac633ac2ecd9ee51263923a4c1dfd2fa377d6c
Ming Qi <s10257808@connect.np.edu.sg>
2025-02-02 05:05:44 +0800

    corrected port

--- src/components/CampaignScheduler.jsx
+++ src/components/CampaignScheduler.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './campaignScheduler.css';
+import '../styles/campaignScheduler.css';
 
 const CampaignScheduler = () => {
   const [schedule, setSchedule] = useState({
