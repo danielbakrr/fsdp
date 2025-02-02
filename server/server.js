c3e3ad109074bdec98c2e07ec35e4e55cb2c3db4
Ming Qi <s10257808@connect.np.edu.sg>
2025-02-02 20:48:51 +0800

    modified cors

--- server/server.js
+++ server/server.js
@@ -53,8 +53,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://githubbies.onrender.com'],
+    origin: ['http://localhost:3000', 'https://githubbies.onrender.com'], // Allowed origins
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
@@ -66,9 +67,10 @@ const io = new Server(server, {
   cors: {
     origin: ['http://localhost:3000', 'https://githubbies.onrender.com'],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   },
 });
-
 
 io.on("connection", (socket) => {
   console.log("User Connected:", socket.id);
